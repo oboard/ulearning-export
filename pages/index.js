b22409d7cf5070643f903dfacae5c182a8d713ca
oboard <oboard@outlook.com>
2023-06-17 20:57:54 +0800

    auto fix answer

--- pages/index.js
+++ pages/index.js
@@ -49,57 +49,65 @@ export default function Home() {
     mode: "cors",
     credentials: "include",
   };
+  let answerMap = {};
+  let ocId, orgId;
+  let answers = {};
+  let qtId;
   const loadQuestion = async (e) => {
     e.preventDefault();
     // 获取题目数量
     console.log(authorization);
 
-    const listResponse = await fetch(`/utestapi/questionTraining/list?ocId=${courseId}&lang=zh`, {
-      "headers": {
-        "accept": "*/*",
-        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-        "authorization": authorization,
-        "cache-control": "no-cache",
-        "content-type": "application/json",
-        "pragma": "no-cache",
-        "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"macOS\"",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-site"
-      },
-      "referrer": "https://courseweb.ulearning.cn/ulearning/index.html",
-      "referrerPolicy": "strict-origin-when-cross-origin",
-      "body": null,
-      "method": "GET",
-      "mode": "cors",
-      "credentials": "include"
-    });
+    const listResponse = await fetch(
+      `/utestapi/questionTraining/list?ocId=${courseId}&lang=zh`,
+      {
+        headers: {
+          accept: "*/*",
+          "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+          authorization: authorization,
+          "cache-control": "no-cache",
+          "content-type": "application/json",
+          pragma: "no-cache",
+          "sec-ch-ua":
+            '" Not A;Brand";v="99", "Chromium";v="112", "Google Chrome";v="112"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+        referrer: "https://courseweb.ulearning.cn/ulearning/index.html",
+        referrerPolicy: "strict-origin-when-cross-origin",
+        body: null,
+        method: "GET",
+        mode: "cors",
+        credentials: "include",
+      }
+    );
 
-  //   {
-  //     "code": 1,
-  //     "message": "成功",
-  //     "result": [
-  //         {
-  //             "id": 1511,
-  //             "ocId": 120798,
-  //             "orgId": 3755,
-  //             "state": 1,
-  //             "createTime": 1685500773000,
-  //             "updateTime": 1685500868000
-  //         }
-  //     ]
-  // }
+    //   {
+    //     "code": 1,
+    //     "message": "成功",
+    //     "result": [
+    //         {
+    //             "id": 1511,
+    //             "ocId": 120798,
+    //             "orgId": 3755,
+    //             "state": 1,
+    //             "createTime": 1685500773000,
+    //             "updateTime": 1685500868000
+    //         }
+    //     ]
+    // }
     const listJson = await listResponse.json();
     if (listJson.code >= 2000) {
       setQuestions(listJson.message);
       return;
     }
     console.log(listJson);
-    const { ocId, orgId, id } = listJson.result[0];
-    const qtId = id;
-
+    ocId = listJson.result[0].ocId;
+    orgId = listJson.result[0].orgId;
+    qtId = listJson.result[0].id;
 
     const answerConditionResponse = await fetch(
       `/utestapi/questionTraining/student/training?qtId=${qtId}&ocId=${ocId}&qtType=1&traceId=${traceId}`,
@@ -132,57 +140,62 @@ export default function Home() {
       `/utestapi/questionTraining/student/answerSheet?qtId=${qtId}&ocId=${ocId}&qtType=1&traceId=${traceId}`,
       config
     );
-    const answers = await answersResponse.json();
-  //   {
-  //     "code": 1,
-  //     "message": "成功",
-  //     "result": {
-  //         "version": 1,
-  //         "state": 1,
-  //         "index": 0,
-  //         "total": 578,
-  //         "list": [
-  //             {
-  //                 "id": 103488,
-  //                 "questionType": 1,
-  //                 "correct": true,
-  //                 "answer": [
-  //                     "A"
-  //                 ]
-  //             },
-  //             {
-  //                 "id": 103489,
-  //                 "questionType": 1,
-  //                 "correct": true,
-  //                 "answer": [
-  //                     "A"
-  //                 ]
-  //             },
-  //             {
-  //                 "id": 103493,
-  //                 "questionType": 1,
-  //                 "correct": null,
-  //                 "answer": null
-  //             },
-  //             {
-  //                 "id": 103497,
-  //                 "questionType": 1,
-  //                 "correct": null,
-  //                 "answer": null
-  //             },
-  //             {
-  //                 "id": 103498,
-  //                 "questionType": 1,
-  //                 "correct": null,
-  //                 "answer": null
-  //             },
-  //         ]
-  //     }
-  // }
-  // 整理成map
-    const answerMap = {};
+    answers = await answersResponse.json();
+    //   {
+    //     "code": 1,
+    //     "message": "成功",
+    //     "result": {
+    //         "version": 1,
+    //         "state": 1,
+    //         "index": 0,
+    //         "total": 578,
+    //         "list": [
+    //             {
+    //                 "id": 103488,
+    //                 "questionType": 1,
+    //                 "correct": true,
+    //                 "answer": [
+    //                     "A"
+    //                 ]
+    //             },
+    //             {
+    //                 "id": 103489,
+    //                 "questionType": 1,
+    //                 "correct": true,
+    //                 "answer": [
+    //                     "A"
+    //                 ]
+    //             },
+    //             {
+    //                 "id": 103493,
+    //                 "questionType": 1,
+    //                 "correct": null,
+    //                 "answer": null
+    //             },
+    //             {
+    //                 "id": 103497,
+    //                 "questionType": 1,
+    //                 "correct": null,
+    //                 "answer": null
+    //             },
+    //             {
+    //                 "id": 103498,
+    //                 "questionType": 1,
+    //                 "correct": null,
+    //                 "answer": null
+    //             },
+    //         ]
+    //     }
+    // }
+    // 整理成map
     answers.result.list.forEach((item) => {
-      answerMap[item.id] = item.answer;
+      if(item.correct){
+        answerMap[item.id] = item.answer;
+      } else {
+        if(item.answer){
+          answerMap[item.id] = 1;
+        }
+      }
     });
     console.log(answers);
 
@@ -209,7 +222,6 @@ export default function Home() {
               ". " +
               item2.title +
               "\n";
-            
           });
           break;
         case 3:
@@ -218,10 +230,12 @@ export default function Home() {
           ques += "正确 错误\n";
           break;
       }
-      if(answerMap[item.id]===null) {
+      if (answerMap[item.id] === 1) {
+        ques += "答案错误\n";
+      } else if(answerMap[item.id] === undefined){
         ques += "未答题\n";
       } else {
-        ques += "答案：" + answerMap[item.id].join(",") + "\n";
+        ques += "答案：" + answerMap[item.id].join("") + "\n";
       }
       ques += "\n";
       // {
@@ -277,6 +291,141 @@ export default function Home() {
       .replace(/<[^>]+>/g, ""); // 去掉所有的html标记
     setQuestions(ques);
   };
+  const fixAnswer = async (e) => {
+    e.preventDefault();
+    await loadQuestion(e);
+    // 循环，检查答案是否错误，如果错误，发送请求，其中请求的body的answer为发送的答案，响应的result.correctAnswer为正确答案
+    // 响应：
+    // {
+    //   "code": 1,
+    //   "message": "回答正确",
+    //   "result": {
+    //     "id": null,
+    //     "subQuestions": null,
+    //     "correctAnswer": [
+    //       "B"
+    //     ],
+    //     "correctreply": "",
+    //     "analyLinkList": null,
+    //     "answerLinkList": null,
+    //     "correct": true
+    //   }
+    // }
+    // 请求：
+    //     fetch("https://utestapi.ulearning.cn/questionTraining/student/answer?traceId=11735745", {
+    //   "headers": {
+    //     "accept": "application/json, text/plain, */*",
+    //     "accept-language": "zh",
+    //     "authorization": "5D4CED0795511D9216C79CB8734EAC13",
+    //     "cache-control": "no-cache",
+    //     "content-type": "application/json;charset=UTF-8",
+    //     "pragma": "no-cache",
+    //     "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\"",
+    //     "sec-ch-ua-mobile": "?0",
+    //     "sec-ch-ua-platform": "\"macOS\"",
+    //     "sec-fetch-dest": "empty",
+    //     "sec-fetch-mode": "cors",
+    //     "sec-fetch-site": "same-site"
+    //   },
+    //   "referrer": "https://utest.ulearning.cn/index.html?v=1687000809024",
+    //   "referrerPolicy": "strict-origin-when-cross-origin",
+    //   "body": "{\"qtId\":1511,\"qtType\":1,\"index\":2,\"relationId\":103493,\"answer\":[\"B\"]}",
+    //   "method": "POST",
+    //   "mode": "cors",
+    //   "credentials": "include"
+    // });
+    setQuestions("正在修复答案...");
+    for(let i in answers.result.list) {
+      let item = answers.result.list[i];
+      if(!item.correct){
+          let answer = item.answer;
+          if(!answer) {
+            answer = ['A'];
+          }
+          // 转换 `{\"qtId\":${qtId},\"qtType\":1,\"index\":2,\"relationId\":${item.id},\"answer\":${answer}}`
+          let ansbody = JSON.stringify({
+            "qtId": qtId,
+            "qtType": 1,
+            "index": i,
+            "relationId": item.id,
+            "answer": [
+              "C"
+            ]
+          });
+          let res = await fetch(
+            `/utestapi/questionTraining/student/answer?traceId=${traceId}`,
+            {
+              headers: {
+                accept: "application/json, text/plain, */*",
+                "accept-language": "zh",
+                authorization: authorization,
+                "cache-control": "no-cache",
+                "content-type": "application/json;charset=UTF-8",
+                pragma: "no-cache",
+                "sec-ch-ua":
+                  '" Not A;Brand";v="99", "Chromium";v="112", "Google Chrome";v="112"',
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": '"macOS"',
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-site",
+              },
+              referrer: "https://utest.ulearning.cn/index.html?v=1687000809024",
+              referrerPolicy: "strict-origin-when-cross-origin",
+              body: ansbody,
+              method: "POST",
+              mode: "cors",
+              credentials: "include",
+            }
+          );
+          let resJson = await res.json();
+          console.log(resJson);
+          
+            answerMap[item.id] = answer = resJson.result.correctAnswer;
+            ansbody = JSON.stringify({
+              "qtId": qtId,
+              "qtType": 1,
+              "index": i,
+              "relationId": item.id,
+              "answer": answer
+            });
+            let json =await (await fetch(
+              `/utestapi/questionTraining/student/answer?traceId=${traceId}`,
+              {
+                headers: {
+                  accept: "application/json, text/plain, */*",
+                  "accept-language": "zh",
+                  authorization: authorization,
+                  "cache-control": "no-cache",
+                  "content-type": "application/json;charset=UTF-8",
+                  pragma: "no-cache",
+                  "sec-ch-ua":
+                    '" Not A;Brand";v="99", "Chromium";v="112", "Google Chrome";v="112"',
+                  "sec-ch-ua-mobile": "?0",
+                  "sec-ch-ua-platform": '"macOS"',
+                  "sec-fetch-dest": "empty",
+                  "sec-fetch-mode": "cors",
+                  "sec-fetch-site": "same-site",
+                },
+                referrer: "https://utest.ulearning.cn/index.html?v=1687000809024",
+                referrerPolicy: "strict-origin-when-cross-origin",
+                body: ansbody,
+                method: "POST",
+                mode: "cors",
+                credentials: "include",
+              }
+            )).json();
+            console.log(json);
+            // 显示修复进度
+            setQuestions( `正在修复答案...第${i}/${answers.result.list.length}题 ${json.result.correctAnswer}`);
+      }
+    }
+    
+    setQuestions("正在导出题库...");
+    // 导出题库
+    await loadQuestion(e);
+  };
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-8 `}
@@ -326,6 +475,9 @@ export default function Home() {
         />
         <button className="w-64 btn" onClick={loadQuestion}>
           导出题库
+        </button>
+        <button className="w-64 btn" onClick={fixAnswer}>
+          修正错误答案
         </button>
       </form>
       {/* {
